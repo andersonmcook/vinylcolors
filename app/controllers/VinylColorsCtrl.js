4b3a57e47d0ddd3b7a117793a9d54d0cc9698010
Anderson Cook <andersoncook@Andersons-MacBook-Pro.local>
2015-12-11 09:29:36 -0600

    starting to work with d path relations in firebase and angular for the secondary layer

--- app/controllers/VinylColorsCtrl.js
+++ app/controllers/VinylColorsCtrl.js
@@ -7,7 +7,9 @@ app.controller("VinylColorsCtrl",
 	$scope.LabelColor = $routeParams.LabelColor;
 
 	var colorsRef = new Firebase("https://vinylcolors.firebaseio.com/colors/");
+	var configsRef = new Firebase("https://vinylcolors.firebaseio.com/configs");
 	$scope.colors = $firebaseArray(colorsRef);
+	$scope.configs = $firebaseArray(configsRef);
 	console.log("colors", $scope.colors);
 	console.log("hey, you're in VinylColors");
 	// $scope.songs = $firebaseArray(ref);
