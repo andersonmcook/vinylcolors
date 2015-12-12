88c870540887b0d609301f82f4b624935687b58f
Anderson Cook <andersoncook@Andersons-MacBook-Pro.local>
2015-12-11 22:47:51 -0600

    made it so there is only one firebase call

--- app/controllers/VinylColorsCtrl.js
+++ app/controllers/VinylColorsCtrl.js
@@ -6,12 +6,18 @@ app.controller("VinylColorsCtrl",
 	$scope.SecondaryColor = $routeParams.SecondaryColor;
 	$scope.LabelColor = $routeParams.LabelColor;
 
-	var colorsRef = new Firebase("https://vinylcolors.firebaseio.com/colors/");
-	var configsRef = new Firebase("https://vinylcolors.firebaseio.com/configs");
+	var ref = new Firebase("http://vinylcolors.firebaseio.com");
+	// var colorsRef = new Firebase("https://vinylcolors.firebaseio.com/colors/");
+	// var configsRef = new Firebase("https://vinylcolors.firebaseio.com/configs");
+	var colorsRef = ref.child("colors");
+	var configsRef = ref.child("configs");
 	$scope.colors = $firebaseArray(colorsRef);
 	$scope.configs = $firebaseArray(configsRef);
-	console.log("colors", $scope.colors);
-	console.log("hey, you're in VinylColors");
+
+	$scope.Configuration = $scope.configs[1];
+
+	// console.log("colors", $scope.colors);
+	// console.log("hey, you're in VinylColors");
 	// $scope.songs = $firebaseArray(ref);
 
 	// $scope.addToUrl = function () {
