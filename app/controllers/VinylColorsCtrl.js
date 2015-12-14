abd4f6a8d339da656b17f9f5eeed091dce49e39e
Anderson Cook <andersonmcook@gmail.com>
2015-12-14 09:42:22 -0600

    reverse button and random button functionality

--- app/controllers/VinylColorsCtrl.js
+++ app/controllers/VinylColorsCtrl.js
@@ -2,9 +2,10 @@ app.controller("VinylColorsCtrl",
 	["$scope", "$routeParams", "$firebaseArray", 
 	function($scope, $routeParams, $firebaseArray) {
 
-	$scope.BaseColor = $routeParams.BaseColor;
-	$scope.SecondaryColor = $routeParams.SecondaryColor;
-	$scope.LabelColor = $routeParams.LabelColor;
+	// $scope.BaseColor = $routeParams.BaseColor;
+	// $scope.SecondaryColor = $routeParams.SecondaryColor;
+	// // $scope.LabelColor = $routeParams.LabelColor;
+	// $scope.Configuration = $routeParams.Configuration;
 
 // pulls from firebase and makes arrays of the objects we need
 	var ref = new Firebase("http://vinylcolors.firebaseio.com");
@@ -31,7 +32,29 @@ app.controller("VinylColorsCtrl",
 
 // grabs URL, not currently functional
 	$scope.addToURL = function () {
-  	$scope.sampleURL = "http://localhost:8080/#/main/" + $scope.BaseColor + "/";
+  	$scope.sampleURL = "http://localhost:8080/#/main/" + $scope.BaseColor.$id + "/" + $scope.SecondaryColor.$id + "/" + $scope.Configuration.$id;
+  	// this logs a hex value with #
+  	console.log("label color", $scope.LabelColor);
+	};
+
+// reverses colors of selections
+	$scope.reverseColors = function () {
+		var a = $scope.BaseColor;
+		var b = $scope.SecondaryColor;
+		$scope.BaseColor = b;
+		$scope.SecondaryColor = a;
+	};
+
+// randomizes colors and configurations
+	$scope.random = function () {
+		var base = Math.floor(Math.random() * ($scope.colors.length));
+		var second = Math.floor(Math.random() * ($scope.colors.length));
+		// var label = Math.floor(Math.random() * ($scope.colors.length - 1));
+		var combo = Math.floor(Math.random() * ($scope.configs.length));
+		$scope.BaseColor = $scope.colors[base];
+		$scope.SecondaryColor = $scope.colors[second];
+		// $scope.LabelColor = label;
+		$scope.Configuration = $scope.configs[combo];
 	};
 
 
